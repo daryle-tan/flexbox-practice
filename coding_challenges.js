ab1adadc44aac0cd4b0b62f54ba54e0d40330449
daryle <daryle.tan@gmail.com>
2024-07-20 17:23:51 -0700

    challenge 3 & 4

--- coding_challenges.js
+++ coding_challenges.js
@@ -18,7 +18,7 @@ function StringChallenge(str) {
   }
 }
 
-//#2
+//#2 (2 wrong)
 function StringChallenge(str) {
   let indexA = 1
   let indexB = 0
@@ -38,3 +38,64 @@ function StringChallenge(str) {
     return false
   }
 }
+
+//#3
+function ArrayChallenge(arr) {
+  let sequenceChange = 0
+  let increase = false
+  let decrease = false
+  let change = false
+
+  if (arr.length < 3) {
+    return -1
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1] && !decrease) {
+      change = true
+      decrease = true
+      sequenceChange += i
+    } else if (arr[i] < arr[i + 1] && !increase) {
+      change = true
+      increase = true
+      sequenceChange += i
+    }
+  }
+
+  if (change && decrease && increase) {
+    return sequenceChange
+  } else {
+    return -1
+  }
+}
+
+//#4
+function ArrayChallenge(arr) {
+  if (meanHelper(arr) === modeHelper(arr)) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+function meanHelper(arr) {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum / arr.length
+}
+
+function modeHelper(arr) {
+  let count = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      count = arr[i]
+    } else if (arr[i] === arr[i - 1]) {
+      count = arr[i]
+    }
+  }
+  return count
+}
+
+//#5
