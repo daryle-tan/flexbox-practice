72c4f240b550b7abc375cce2b9abc0a35d0e5f15
daryle <daryle.tan@gmail.com>
2024-09-11 22:16:22 -0500

    palindrome

--- coder_week3.py
+++ coder_week3.py
@@ -1,4 +1,4 @@
-# Letter Changes
+# String Scramble
 def StringScramble(str1,str2):
   match = []
   for i, letter in enumerate(str2):
@@ -13,3 +13,27 @@ def StringScramble(str1,str2):
 
 # keep this function call here 
 print(StringScramble(input()))
+
+# Palindrome
+def PalindromeTwo(strParam):
+  match = False
+  strContainer = []
+  for letter in strParam:
+    if letter.isalpha():
+      strContainer.append(letter)
+  newStr = "".join(strContainer)
+
+  reversed_string = newStr[::-1].lower()
+  lower_string = newStr.lower()
+  print(reversed_string, "r", lower_string, "l")
+  for i, char in enumerate(lower_string):
+    if lower_string[i] == reversed_string[i]:
+      match = True
+    else:
+     return False
+  return match
+
+# keep this function call here 
+print(PalindromeTwo(input()))
+
+# Counting Minutes
